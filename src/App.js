6e81ef5288ebeda966cb4a24ece57bc5cc259a3e
Jorge <jorrillop@hotmail.com>
2019-11-11 18:52:22 +0100

    frontend auth done

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+
+import Navbar from './components/NavBar.js';
+import Private from './pages/Private';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import AuthProvider from './context/AuthContext.js'
+import PrivateRoute from './components/PrivateRoute'
+import AnonRoute from './components/AnonRoute'
+import './App.css';
+import 'milligram';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <AuthProvider>
+        <div className="container">
+          <h1>Basic React Authentization</h1>
+          <Navbar />
+          <Switch>
+            <AnonRoute path="/signup" component={Signup} />
+            <AnonRoute path="/login" component={Login} />
+            <PrivateRoute path="/private" component={Private} />
+          </Switch>
+        </div>
+        </AuthProvider>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ src/pages/Private.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import withAuth from '../components/withAuth.js'
+class Private extends Component {
+  render() {
+    
+    return (
+      <div>
+        <h1>Welcome to {this.props.user.username}</h1>
+      </div>
+    )
+  }
+}
+
+export default  withAuth(Private);
